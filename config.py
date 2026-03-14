954a6a1c683be2e3b805b98112b7624abe699769
office612 <office@salesdoc.io>
2026-03-14 17:17:24 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ZVS_TOKEN = os.getenv("ZVS_TOKEN", "")
+ZVS_SID = os.getenv("ZVS_SID", "1n-5gFiCHYUE5fRR6Flj_ZhO-cMFrf-gFocPQPlvrUd4")
+ZVS_DIR = os.getenv("ZVS_DIR", "5472344802")
+ZVS_GRP = os.getenv("ZVS_GRP", "-1003751206447")
+GOOGLE_CREDS_JSON = os.getenv("GOOGLE_CREDS_JSON", "")
