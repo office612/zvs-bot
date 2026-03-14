d38fb7e14859643ddbfdd2b42ce2ce79f7780238
office612 <office@salesdoc.io>
2026-03-14 17:13:25 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from config import ZVS_TOKEN
+from handlers import zvs
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    bot = Bot(
+        token=ZVS_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(zvs.router)
+    logger.info("ZVS Bot started")
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot, allowed_updates=["callback_query", "message"])
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
